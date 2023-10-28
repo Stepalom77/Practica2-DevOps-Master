227120418a6343726acb0f39ff8a105bdaa4f4c3
Stepalom77 <stepalom22@gmail.com>
2023-10-28 18:34:54 -0500

    Agregando error handling

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,18 +1,26 @@
 def call(boolean abortPipeline = false, def scannerHome) {
-    withSonarQubeEnv(credentialsId: 'sonar-token') {
-        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Threepoints_test \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=http://localhost:9000 \
-            -Dsonar.token=sqp_efae856826fa4b699a9c2ddbe39c2f613a492734"
-        timeout(time: 5, unit: 'MINUTES') {
-            script {
-                def qualityGate = waitForQualityGate()
-                if(qualityGate.status != 'OK') {
-                    if(abortPipeline) {
-                        error "Gate failed ${qualityGate.status}"
+    try {
+        withSonarQubeEnv(credentialsId: 'sonar-token') {
+            echo "Starting SonarQube analysis..."
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Threepoints_test \
+                -Dsonar.sources=. \
+                -Dsonar.host.url=http://localhost:9000 \
+                -Dsonar.token=sqp_efae856826fa4b699a9c2ddbe39c2f613a492734"
+            timeout(time: 5, unit: 'MINUTES') {
+                script {
+                    def qualityGate = waitForQualityGate()
+                    if(qualityGate.status != 'OK') {
+                        if(abortPipeline) {
+                            error "Gate failed ${qualityGate.status}"
+                        }
                     }
                 }
             }
+        }
+    } catch (Exception e) {
+        echo "An error occurred during SonarQube analysis: ${e.getMessage()}"
+        if (abortPipeline) {
+            error "Aborting pipeline due to SonarQube analysis failure."
         }
     }
 }
