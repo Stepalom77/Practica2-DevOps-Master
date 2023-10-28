61c60b719707b7a9f79bfb4a8d02e6f9967ee8ef
Stepalom77 <stepalom22@gmail.com>
2023-10-28 18:26:16 -0500

    Fixing SonarQube

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,6 +1,9 @@
 def call(boolean abortPipeline = false, def scannerHome) {
     withSonarQubeEnv(credentialsId: 'sonar-token') {
-        sh "${scannerHome}/bin/sonar-scanner"
+        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Threepoints_test \
+            -Dsonar.sources=. \
+            -Dsonar.host.url=http://localhost:9000 \
+            -Dsonar.token=sqp_efae856826fa4b699a9c2ddbe39c2f613a492734"
         timeout(time: 5, unit: 'MINUTES') {
             script {
                 def qualityGate = waitForQualityGate()
